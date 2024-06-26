2f97960ad28dd75162d7fd6044a89dde641caacb
raphael sena <rsenares1@gmail.com>
2024-06-25 22:38:59 -0300

    refactor: add estrelas no card repositório

--- code/assets/js/reposGithub.js
+++ code/assets/js/reposGithub.js
@@ -37,6 +37,8 @@ document.addEventListener("DOMContentLoaded", () => {
             <div class="card-body">
               <h5 class="card-title">${repo.name}</h5>
               <p class="card-text">${repo.description ? repo.description : 'Sem descrição'}</p>
+            <p><i class="fas fa-star">&nbsp</i>${repo.stargazers_count}</p>
+
             </div>
             <div class="card-footer">
               <button class="btn btn-dark" onclick="viewRepoDetails('${repo.name}')">Ver Detalhes</button>
