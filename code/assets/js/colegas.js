6678f0f0e7bfb82b852093ba7c470f25b210df43
raphael sena <rsenares1@gmail.com>
2024-06-26 10:35:59 -0300

    feat: section Colegas

---
+++ code/assets/js/colegas.js
@@ -0,0 +1,22 @@
+document.addEventListener('DOMContentLoaded', function() {
+    fetch('http://localhost:3000/colegas')
+      .then(response => response.json())
+      .then(data => {
+        const colegasList = document.getElementById('colegas-list');
+        data.forEach(colega => {
+          const colegaCard = document.createElement('div');
+          colegaCard.className = 'col-md-4';
+          colegaCard.innerHTML = `
+            <div class="card mb-4 shadow-sm">
+              <img src="${colega.imagem}" class="card-img-top" alt="${colega.nome}">
+              <div class="card-body">
+                <h5 class="card-title">${colega.nome}</h5>
+              </div>
+            </div>
+          `;
+          colegasList.appendChild(colegaCard);
+        });
+      })
+      .catch(error => console.error('Erro ao carregar os colegas:', error));
+  });
+  
