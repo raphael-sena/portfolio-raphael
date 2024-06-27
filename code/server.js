5d3cb4327ed64d841a1522ac9a599999a7a53b82
raphael sena <rsenares1@gmail.com>
2024-06-27 11:45:49 -0300

    fix: vercel

--- code/server.js
+++ code/server.js
@@ -1,17 +1,24 @@
-const jsonServer = require('json-server')
-const server = jsonServer.create()
+// JSON Server module
+const jsonServer = require("json-server");
+const server = jsonServer.create();
+const router = jsonServer.router("db/db.json");
 
-const fs = require('fs')
-const path = require('path')
-const filePath = path.join('data/db.json')
-const data = fs.readFileSync(filePath, "utf-8");
-const db = JSON.parse(data);
-const router = jsonServer.router(db)
+// Make sure to use the default middleware
+const middlewares = jsonServer.defaults();
 
-const middlewares = jsonServer.defaults()
-
-server.use(middlewares)
-server.use(router)
+server.use(middlewares);
+// Add this before server.use(router)
+server.use(
+ // Add custom route here if needed
+ jsonServer.rewriter({
+  "/api/*": "/$1",
+ })
+);
+server.use(router);
+// Listen to port
 server.listen(3000, () => {
-  console.log('JSON Server is running')
-})
+ console.log("JSON Server is running");
+});
+
+// Export the Server API
+module.exports = server;
