004ec98866555d1785ca800bd43a160d908648ec
raphael sena <rsenares1@gmail.com>
2024-06-27 12:38:58 -0300

    fix: vercel deployment

--- code/server.js
+++ code/server.js
@@ -1,24 +1,21 @@
-// JSON Server module
-const jsonServer = require("json-server");
-const server = jsonServer.create();
-const router = jsonServer.router("data/db.json");
+const jsonServer = require('json-server');
+const path = require('path');
 
-// Make sure to use the default middleware
+const server = jsonServer.create();
+const router = jsonServer.router(path.join(__dirname, 'data', 'db.json'));
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
-// Add this before server.use(router)
 server.use(
- // Add custom route here if needed
- jsonServer.rewriter({
-  "/api/*": "/$1",
- })
+  jsonServer.rewriter({
+    '/api/*': '/$1',
+  })
 );
 server.use(router);
-// Listen to port
-server.listen(3000, () => {
- console.log("JSON Server is running");
+
+const port = process.env.PORT || 3000;
+server.listen(port, () => {
+  console.log(`JSON Server is running on port ${port}`);
 });
 
-// Export the Server API
 module.exports = server;
