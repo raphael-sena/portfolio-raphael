bed3df0d76fe9fc53db2bfbb25026a5027773fb2
raphael sena <rsenares1@gmail.com>
2024-06-27 13:08:28 -0300

    fix: server.js

--- code/server.js
+++ code/server.js
@@ -1,21 +1,17 @@
-const jsonServer = require('json-server');
-const path = require('path');
+const jsonServer = require('json-server')
+const server = jsonServer.create()
 
-const server = jsonServer.create();
-const router = jsonServer.router(path.join(__dirname, 'data', 'db.json'));
-const middlewares = jsonServer.defaults();
+const fs = require('fs')
+const path = require('path')
+const filePath = path.join('data/db.json')
+const data = fs.readFileSync(filePath, "utf-8");
+const db = JSON.parse(data);
+const router = jsonServer.router(db)
 
-server.use(middlewares);
-server.use(
-  jsonServer.rewriter({
-    '/api/*': '/$1',
-  })
-);
-server.use(router);
+const middlewares = jsonServer.defaults()
 
-const port = process.env.PORT || 3000;
-server.listen(port, () => {
-  console.log(`JSON Server is running on port ${port}`);
-});
-
-module.exports = server;
+server.use(middlewares)
+server.use(router)
+server.listen(3000, () => {
+  console.log('JSON Server is running')
+})
